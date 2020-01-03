dd72bcdfbb3df19793d08fdd29a916dbe58c866e
Diogo Ferraz <Diogo.Ferraz@vision-box.com>
2020-01-03 11:22:08 +0000

    Implement PORT to server

--- server.js
+++ server.js
@@ -4,6 +4,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 
 app.engine('.html', require('ejs').__express);
@@ -20,7 +22,7 @@ app.get('*', (req, res) => {
   res.render(`${__dirname}/dist/index.html`);
 });
 
-const server = app.listen(3000, () => {});
+const server = app.listen(PORT, () => { });
 const host = server.address().address;
 const { port } = server.address();
 console.log('App listening at http://%s:%s', host, port);
