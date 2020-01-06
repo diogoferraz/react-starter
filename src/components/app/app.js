f798b624ef61866055f566cd089dca4bd0608bf5
Diogo Ferraz <Diogo.Ferraz@vision-box.com>
2020-01-06 17:34:44 +0000

    Initial implementation for menu

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,16 +2,20 @@ import React from 'react';
 import { hot } from 'react-hot-loader/root';
 import { useSelector, useDispatch } from '../../hooks/react-redux';
 import messageActions from '../../actions/messageActions';
+import MenuDrawer from '../menu-drawer/menu-drawer';
 import styles from './styles';
 
 const App = () => {
   const dispatch = useDispatch();
   const { message } = useSelector((state) => ({ message: state.messageReducer.message }));
   return (
-    <div>
-      <div className={styles.intro}>Hello World!</div>
-      <button type="submit" onClick={() => dispatch(messageActions.success('Success!'))}>Click here to dispatch success message</button>
-      {message}
+    <div className={styles.main}>
+      <MenuDrawer />
+      <div>
+        <div className={styles.intro}>Hello World!</div>
+        <button type="submit" onClick={() => dispatch(messageActions.success('Success!'))}>Click here to dispatch success message</button>
+        {message}
+      </div>
     </div>
   );
 };
---
+++ src/components/menu-drawer/menu-drawer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './styles';
+
+const MenuDrawer = () => (
+  <div className={styles.container}>
+    <p>Menu</p>
+  </div>
+);
+
+export default MenuDrawer;
